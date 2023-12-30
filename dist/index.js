cbbc8aeda38eda6c6afe239070702a76c522bbce
Anas Obaid <anas@ocyber.work>
2023-12-30 19:27:23 +0500

    Cannot create empty task.

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,38 @@
 "use strict";
 function addTask(title) {
     var _a;
-    const tasksList = document.getElementsByTagName("ul");
-    const item = document.createElement("li");
-    const taskLabel = document.createElement("label");
-    const button = document.createElement("button");
-    const div = document.createElement("div");
-    div.className = "output-group";
-    taskLabel.className = "label";
-    taskLabel.htmlFor = "taskCheck";
-    taskLabel.textContent = title;
-    button.className = "btn btn--danger";
-    button.textContent = "Delete";
-    button.addEventListener("click", () => {
-        const isConfirmed = confirm("Are you sure?");
-        if (isConfirmed) {
-            item.remove();
-        }
-    });
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.id = "taskCheck";
-    const task = document.createElement("div");
-    task.className = "task";
-    task.appendChild(checkbox);
-    task.appendChild(taskLabel);
-    div.appendChild(task);
-    div.append(button);
-    item.appendChild(div);
-    (_a = tasksList[0]) === null || _a === void 0 ? void 0 : _a.append(item);
-    const inputField = document.getElementsByTagName("input")[0];
-    inputField.value = "";
+    if (title !== "") {
+        const tasksList = document.getElementsByTagName("ul");
+        const item = document.createElement("li");
+        const taskLabel = document.createElement("label");
+        const button = document.createElement("button");
+        const div = document.createElement("div");
+        div.className = "output-group";
+        taskLabel.className = "label";
+        taskLabel.htmlFor = "taskCheck";
+        taskLabel.textContent = title;
+        button.className = "btn btn--danger";
+        button.textContent = "Delete";
+        button.addEventListener("click", () => {
+            const isConfirmed = confirm("Are you sure?");
+            if (isConfirmed) {
+                item.remove();
+            }
+        });
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = "taskCheck";
+        const task = document.createElement("div");
+        task.className = "task";
+        task.appendChild(checkbox);
+        task.appendChild(taskLabel);
+        div.appendChild(task);
+        div.append(button);
+        item.appendChild(div);
+        (_a = tasksList[0]) === null || _a === void 0 ? void 0 : _a.append(item);
+        const inputField = document.getElementsByTagName("input")[0];
+        inputField.value = "";
+    }
 }
 function handleTextChange() {
     var _a;
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,41 @@
 function addTask(title: string): void {
-  const tasksList = document.getElementsByTagName("ul");
-  const item = document.createElement("li");
-  const taskLabel = document.createElement("label");
-  const button = document.createElement("button");
-  const div = document.createElement("div");
+  if (title !== "") {
+    const tasksList = document.getElementsByTagName("ul");
+    const item = document.createElement("li");
+    const taskLabel = document.createElement("label");
+    const button = document.createElement("button");
+    const div = document.createElement("div");
 
-  div.className = "output-group";
-  taskLabel.className = "label";
-  taskLabel.htmlFor = "taskCheck";
-  taskLabel.textContent = title;
-  button.className = "btn btn--danger";
-  button.textContent = "Delete";
-  button.addEventListener("click", () => {
-    const isConfirmed = confirm("Are you sure?");
-    if (isConfirmed) {
-      item.remove();
-    }
-  });
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.id = "taskCheck";
+    div.className = "output-group";
+    taskLabel.className = "label";
+    taskLabel.htmlFor = "taskCheck";
+    taskLabel.textContent = title;
+    button.className = "btn btn--danger";
+    button.textContent = "Delete";
+    button.addEventListener("click", () => {
+      const isConfirmed = confirm("Are you sure?");
+      if (isConfirmed) {
+        item.remove();
+      }
+    });
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "taskCheck";
 
-  const task = document.createElement("div");
-  task.className = "task";
+    const task = document.createElement("div");
+    task.className = "task";
 
-  task.appendChild(checkbox);
-  task.appendChild(taskLabel);
+    task.appendChild(checkbox);
+    task.appendChild(taskLabel);
 
-  div.appendChild(task);
-  div.append(button);
-  item.appendChild(div);
-  tasksList[0]?.append(item);
+    div.appendChild(task);
+    div.append(button);
+    item.appendChild(div);
+    tasksList[0]?.append(item);
 
-  const inputField = document.getElementsByTagName("input")[0];
-  inputField.value = "";
+    const inputField = document.getElementsByTagName("input")[0];
+    inputField.value = "";
+  }
 }
 
 function handleTextChange(): string {
